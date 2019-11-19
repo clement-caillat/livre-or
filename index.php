ef946cd54573216eb68fead635f86be3ce50cb4b
Clément Caillat <clement.caillat@laplateforme.io>
2019-11-19 22:45:37 +0100

    Version 0.2

--- index.php
+++ index.php
@@ -1,33 +1,37 @@
+<?php $login="Sweetar" ?>
+
 <!DOCTYPE html>
 <html lang="fr">
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <link rel="stylesheet" href="style.css">
-    <link rel="icon" href="images/icons/logo.png">
-    <title>Tomorrowland Winter 2019 | Accueil</title>
+    <link rel="icon" href="ressources/images/icons/logo.png">
+    <title>Tomorrowland Winter 2019</title>
 </head>
 <body>
     <nav>
+    <audio autoplay loop>
+        <source src="ressources/audio/intro.mp3" type="audio/mp3">
+    </audio>
         <div id="user">
-            <img src="images/divers/papillon.jpg">
-            <h2>Visiteur</h2>
+            <img src="ressources/images/divers/papillon.jpg">
+            <?php if(isset($login)){echo "<a style=\"text-decoration: none;\"href=\"profil.php\"><h2>$login</h2></a>";} ?>
         </div>
         <div id="liens">
             <a href="">Accueil</a>
-            <a href="">Inscription/Connexion</a>
-            <a href="">Top 10 DJ'S</a>
+            <a href="">Top 5 DJ'S</a>
             <a href="">Livre d'or</a>
-            <a href="">Fil d'actualités</a>
-            <a href="">Se déconnecter</a>
+            <a href="">Poster un commentaire</a>
+            <?php if(isset($login)){echo "<a href=\"\" style=\"color: red;\">Se déconnecter</a>";}else{ echo "<a href=\"login.php\">Inscription/Connexion</a>";} ?>
         </div>
     </nav>
-    <main>
+    <main id="main_index">
         <header id="haut">
-            <a href="index.php#bas"><img src="images/divers/arrow.png"></a>
+            <a href="index.php#bas"><img src="ressources/images/divers/arrow.png"></a>
         </header>
         <section id="bas">
-            <a href="index.php#haut"><img src="images/divers/arrow.png"></a>
+            <a href="index.php#haut"><img src="ressources/images/divers/arrow.png"></a>
             <article>
                 <div id="title">
                     <h1>Tomorrowland Winter 2019 Edition</h1>
---
+++ register.php
@@ -0,0 +1,17 @@
+<!DOCTYPE html>
+<html lang="fr">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" href="style.css">
+    <link rel="icon" href="images/icons/logo.png">
+    <title>Inscription | Tomorrowland Winter 2019</title>
+</head>
+<body>
+    <main id="register_content">
+        <form action="register.php" method="post">
+            <input type="text" name="username" placeholder="Nom d'utilisateur" required>
+        </form>
+    </main>
+</body>
+</html>
