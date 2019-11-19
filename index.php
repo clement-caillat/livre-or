f759814ba8eafbb1aca5bda6c2876a94525a2cba
Clément Caillat <clement-caillat@laplateforme.io>
2019-11-19 20:52:21 +0100

    Version 0.1

---
+++ index.php
@@ -0,0 +1,39 @@
+<!DOCTYPE html>
+<html lang="fr">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" href="style.css">
+    <link rel="icon" href="images/icons/logo.png">
+    <title>Tomorrowland Winter 2019 | Accueil</title>
+</head>
+<body>
+    <nav>
+        <div id="user">
+            <img src="images/divers/papillon.jpg">
+            <h2>Visiteur</h2>
+        </div>
+        <div id="liens">
+            <a href="">Accueil</a>
+            <a href="">Inscription/Connexion</a>
+            <a href="">Top 10 DJ'S</a>
+            <a href="">Livre d'or</a>
+            <a href="">Fil d'actualités</a>
+            <a href="">Se déconnecter</a>
+        </div>
+    </nav>
+    <main>
+        <header id="haut">
+            <a href="index.php#bas"><img src="images/divers/arrow.png"></a>
+        </header>
+        <section id="bas">
+            <a href="index.php#haut"><img src="images/divers/arrow.png"></a>
+            <article>
+                <div id="title">
+                    <h1>Tomorrowland Winter 2019 Edition</h1>
+                </div>
+            </article>
+        </section>
+    </main>
+</body>
+</html>
