89067a18600c20efbc46ffd9b8fb7b429cdd537e
Clément Caillat <clement-caillat@laplateforme.io>
2019-11-21 11:02:54 +0100

    Version finale

--- livreor.sql
+++ livreor.sql
@@ -3,7 +3,7 @@
 -- https://www.phpmyadmin.net/
 --
 -- Hôte : 127.0.0.1:3306
--- Généré le :  mer. 20 nov. 2019 à 18:01
+-- Généré le :  jeu. 21 nov. 2019 à 10:01
 -- Version du serveur :  5.7.26
 -- Version de PHP :  7.2.18
 
@@ -35,16 +35,17 @@ CREATE TABLE IF NOT EXISTS `commentaires` (
   `id_utilisateur` int(11) NOT NULL,
   `date` datetime NOT NULL,
   PRIMARY KEY (`id`)
-) ENGINE=MyISAM AUTO_INCREMENT=4 DEFAULT CHARSET=latin1;
+) ENGINE=MyISAM AUTO_INCREMENT=5 DEFAULT CHARSET=latin1;
 
 --
 -- Déchargement des données de la table `commentaires`
 --
 
 INSERT INTO `commentaires` (`id`, `commentaire`, `id_utilisateur`, `date`) VALUES
-(1, '', 2, '2019-11-20 17:23:53'),
-(2, '', 2, '2019-11-20 17:25:01'),
-(3, 'Coucou Ã  tous !', 2, '2019-11-20 17:29:02');
+(1, 'J\'ai adorÃ© le festival, une ambiance incroyable et le thÃ¨me de cette Ã©dition Ã©tait gÃ©nial !ðŸ’•ðŸ˜ðŸ‘ðŸŽ¶', 1, '2019-11-21 09:53:03'),
+(2, 'J\'ai oubliÃ© de prÃ©ciser, la prochaine fois passez par airbnb pour les logements  ðŸ˜‰', 1, '2019-11-21 09:54:31'),
+(3, 'This was amazing, I really loved the theme of this 2019 Edition. The secourists were very kind. Don\'t bring shrooms because there\'s lot of security...ðŸ˜…ðŸ˜…', 2, '2019-11-21 09:56:29'),
+(4, 'J\'ai vraiment adorÃ© cette Edition c\'Ã©tait incroyable ! La journÃ©e on partait skier, et le soir on kiffait ! ðŸ”ðŸ”', 3, '2019-11-21 09:58:40');
 
 -- --------------------------------------------------------
 
@@ -58,15 +59,17 @@ CREATE TABLE IF NOT EXISTS `utilisateurs` (
   `login` varchar(255) NOT NULL,
   `password` varchar(255) NOT NULL,
   PRIMARY KEY (`id`)
-) ENGINE=MyISAM AUTO_INCREMENT=3 DEFAULT CHARSET=latin1;
+) ENGINE=MyISAM AUTO_INCREMENT=5 DEFAULT CHARSET=latin1;
 
 --
 -- Déchargement des données de la table `utilisateurs`
 --
 
 INSERT INTO `utilisateurs` (`id`, `login`, `password`) VALUES
-(1, 'Sass13', '$2y$10$mtqWNV0oU4d3vjURXVXb2OOEbsAR1bJoJ5P47r4R.fsz0q45tYGhO'),
-(2, 'Sweetar', '$2y$10$wi1R74/tcXYdPEE2VrmPLuw8Co19ZbIJnE7ZWSLmiWEqAzVkfefuC');
+(1, 'Sass13', '$2y$10$ZYdONpKAUtST7rHRONEeUO92Mo3EyyB6iVQZXWyVBxw49g0OpApra'),
+(2, 'Psymaster555', '$2y$10$bMvlR8C5jkoNa2W.f1sL0.CA9cKeExzvhtT0P4.dH3/c.39Fg8vi2'),
+(3, 'Sweetar', '$2y$10$7CGj4sC6Rf/bsF5K6MhIqO3rDX5gOtPiFI4r6/dQdDIOY30rNmWZC'),
+(4, 'jon', '$2y$10$edx7lfZFhvyk2eo6DwM/gexzn5tLxW7o7CK37z6v8F7q3nP5iOEAK');
 COMMIT;
 
 /*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
